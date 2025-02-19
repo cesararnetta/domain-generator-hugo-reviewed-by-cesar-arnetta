daa3c4236888918a8c206d2b2e5c9afb7d8e00e4
hugo9442 <hugo9442@gmail.com>
2025-02-19 20:29:26 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import "bootstrap";
+/*import "bootstrap";
 import "./style.css";
 
 
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
   console.log("Hello Rigo from the console!");
-};
+};*/
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon']
+let exten = ['.com', '.es', '.br.com',]
+
+/*for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let h = 0; h < noun.length; h++) {
+      for (let f = 0; f < exten.length; f++) {
+          console.log(pronoun[i] + adj[j] + noun[h] + exten[f])
+
+    }
+  }
+}
+}*/
+
+//hack
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let h = 0; h < noun.length; h++) {
+      
+          console.log(pronoun[i] + adj[j] + noun[h].slice(0,noun[h].length-2)+'.'+noun[h].slice(noun[h].length-2,noun[h].length))
+
+    
+  }
+}
+}
